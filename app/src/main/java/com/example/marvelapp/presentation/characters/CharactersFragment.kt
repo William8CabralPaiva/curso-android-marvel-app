600503a838c7efae7133cda1a39621d3d1348e71
WilliamCabral <william8cabral@gmail.com.br>
2023-01-03 17:20:44 -0300

    navigation

---
+++ app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
@@ -0,0 +1,29 @@
+package com.example.marvelapp.presentation.characters
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.marvelapp.R
+import com.example.marvelapp.databinding.FragmentCharactersBinding
+
+class CharactersFragment : Fragment() {
+
+    private var _binding: FragmentCharactersBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCharactersBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            CharactersFragment()
+    }
+}
