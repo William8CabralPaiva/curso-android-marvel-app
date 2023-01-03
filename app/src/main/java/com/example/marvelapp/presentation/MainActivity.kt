a7f0c8369f493b7733b2397cebac18569e473f2c
WilliamCabral <william8cabral@gmail.com.br>
2023-01-03 17:20:13 -0300

    navigation

--- app/src/main/java/com/example/marvelapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/marvelapp/presentation/MainActivity.kt
@@ -1,12 +1,47 @@
 package com.example.marvelapp.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.example.marvelapp.R
+import com.example.marvelapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_container) as NavHostFragment
+
+        navController = navHostFragment.navController
+
+        binding.apply {
+            bottomNavMain.setupWithNavController(navController)
+
+            //todo define as rotas principais
+            appBarConfiguration = AppBarConfiguration(setOf(R.id.home, R.id.about, R.id.favorites))
+
+            toolbarApp.setupWithNavController(navController, appBarConfiguration)
+
+            navController.addOnDestinationChangedListener { _, destination, _ ->
+                val isTopLevelDestination =
+                    appBarConfiguration.topLevelDestinations.contains(destination.id)
+
+                if (!isTopLevelDestination) {
+                    toolbarApp.setNavigationIcon(R.drawable.ic_back)
+                }
+            }
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/example/marvelapp/presentation/favorites/FavoritesFragment.kt
@@ -0,0 +1,37 @@
+package com.example.marvelapp.presentation.favorites
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.marvelapp.R
+import com.example.marvelapp.databinding.FragmentCharactersBinding
+import com.example.marvelapp.databinding.FragmentFavoritesBinding
+
+
+class FavoritesFragment : Fragment() {
+
+    private var _binding: FragmentFavoritesBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance() =
+            FavoritesFragment().apply {
+                arguments = Bundle().apply {
+                    //  putString(ARG_PARAM1, param1)
+                    // putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
