51a4d8792927df846d21272fb1ba3e8c02ebf538
WilliamCabral <william8cabral@gmail.com.br>
2023-01-30 20:46:02 -0300

    proguard, minify, ofuscation, versions name config

--- core/src/main/java/com/example/core/data/response/CharacterResponse.kt
+++ core/src/main/java/com/example/core/data/response/CharacterResponse.kt
@@ -1,10 +1,14 @@
 package com.example.core.data.response
 
 import com.example.core.domain.model.Character
+import com.google.gson.annotations.SerializedName
 
 data class CharacterResponse(
+    @SerializedName("id")
     val id: String,
+    @SerializedName("name")
     val name: String,
+    @SerializedName("thumbnail")
     val thumbnail: ThumbnailResponse
 )
 
--- core/src/main/java/com/example/core/data/response/CharacterWrapperResponse.kt
+++ core/src/main/java/com/example/core/data/response/CharacterWrapperResponse.kt
@@ -1,6 +1,10 @@
 package com.example.core.data.response
 
+import com.google.gson.annotations.SerializedName
+
 data class CharacterWrapperResponse(
+    @SerializedName("copyright")
     val copyright: String,
+    @SerializedName("data")
     val data: CharacterContainerResponse
 )
