01677370245c8bd994ba1c0a5abf42b78cf19292
WilliamCabral <william8cabral@gmail.com.br>
2023-03-05 20:31:22 -0300

    fragment correction

--- app/src/androidTest/java/com/example/marvelapp/presentation/characters/CharactersFragmentTest.kt
+++ app/src/androidTest/java/com/example/marvelapp/presentation/characters/CharactersFragmentTest.kt
@@ -1,5 +1,8 @@
 package com.example.marvelapp.presentation.characters
 
+import android.app.Application
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
@@ -14,6 +17,8 @@ import com.example.marvelapp.presentation.characters.adapters.CharacterViewHolde
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.UninstallModules
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
@@ -37,12 +42,18 @@ class CharactersFragmentTest {
 
     private lateinit var server: MockWebServer
 
+    private val navController = TestNavHostController(
+        ApplicationProvider.getApplicationContext()
+    )
+
     @Before
     fun setup() {
         server = MockWebServer().apply {
             start(8080)
         }
-        launchFragmentInHiltContainer<CharactersFragment>() //todo launchFragmentInContainer foi trocada pois estamos usando o Hilt
+        launchFragmentInHiltContainer<CharactersFragment>(
+            navHostController = navController
+        ) //todo launchFragmentInContainer foi trocada pois estamos usando o Hilt
     }
 
     @After
@@ -51,20 +62,22 @@ class CharactersFragmentTest {
     }
 
     @Test
-    fun shouldShowCharacters_whenViewIsCreated() {
+    fun shouldShowCharacters_whenViewIsCreated(): Unit = runBlocking {
         server.enqueue(MockResponse().setBody("characters_p1.json".asJsonString()))
+        delay(500)//delay para dar tempo fazer a configuração do server
         onView(
             withId(R.id.recycle_characters)// ve se o recycle ta visivel
         ).check(matches(isDisplayed()))
     }
 
     @Test
-    fun shouldLoadMoreCharacters_whenPageIsRequested() {
+    fun shouldLoadMoreCharacters_whenPageIsRequested(): Unit = runBlocking {
         //Arrange
         server.apply {
             enqueue(MockResponse().setBody("characters_p1.json".asJsonString()))
             enqueue(MockResponse().setBody("characters_p2.json".asJsonString()))
         }
+        delay(500)
 
         //Action
         onView(
@@ -82,8 +95,9 @@ class CharactersFragmentTest {
     }
 
     @Test
-    fun shouldShowErrorView() {
+    fun shouldShowErrorView(): Unit = runBlocking {
         server.enqueue(MockResponse().setResponseCode(404))
+        delay(500)
 
         onView(
             withId(R.id.text_initial_load_error)// ve se o recycle ta visivel
